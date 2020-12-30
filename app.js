3822e11d6ec034f748697ddb8b7d3fbdb7bffc1a
inquityy1 <nebojsanikolic12350@yahoo.com>
2020-12-30 14:19:05 +0100

    adding some logic for promt and choises

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-/* const start = function() {
-	console.log('Game is starting...');
-};
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-const person = {
-	name: 'Max',
-	greet: function greet() {
-		console.log('Hello there!');
+let gameIsRunning = false;
+
+const getPlayerChoice = function() {
+	const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+	if (
+		selection !== ROCK &&
+		selection !== PAPER &&
+		selection !== SCISSORS
+	) {
+		alert(`Invalid choice! We chose ${ROCK} for you!`);
+		return DEFAULT_USER_CHOICE;
 	}
+	return selection;
 };
-
-person.greet();
-
-console.dir(startGame);*/
 
 startGameBtn.addEventListener('click', function() {
-	console.log('Game is starting...', age);
+	if (gameIsRunning) {
+		return;
+	}
+	gameIsRunning = true;
+	console.log('Game is starting...');
+	const playerSelection = getPlayerChoice();
+	console.log(playerSelection);
 });
