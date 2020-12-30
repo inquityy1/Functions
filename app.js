4a416ba577486b92d8a8fe57023aec8e309810d6
inquityy1 <nebojsanikolic12350@yahoo.com>
2020-12-30 18:31:58 +0100

    learnig about argument i rest operator

--- app.js
+++ app.js
@@ -83,6 +83,28 @@ startGameBtn.addEventListener('click', () => {
 	gameIsRunning = false;
 });
 
+// not related to game-btn
+
+const sumUp = (...numbers) => {
+	let sum = 0;
+	for (const num of numbers) {
+		sum += num;
+	}
+	return sum;
+};
+
+const subtractUp = function() {
+	let sum = 0;
+	for (const num of arguments) {
+		sum += num;
+	}
+	return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
+
 
 
 
